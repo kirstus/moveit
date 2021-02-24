e207aa9ba0ce05194a2c82a993b5ef0984b9e44e
Matheus Kirstus <makirstus@uolinc.com>
2021-02-24 17:51:21 -0300

    Dia 3

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+import '../styles/global.css'
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
+}
+
+export default MyApp
